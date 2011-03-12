6928e584ed60b28a520a65c78e3852c6e85a8b8e
chouti <chouti@gmail.com>
2011-03-12 15:45:38 +0800

    initial commitment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Diditme::Application.config.secret_token = '94f96c15ea1d3777c6d26f6f0a80f87b6b3ff2eb22261c98c63a5295c1b26d70a7dd1b3bd61a01d68411e6585f805d0a16f21183c0ee47fdc460027370b5c812'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Diditme::Application.config.session_store :cookie_store, :key => '_diditme_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Diditme::Application.config.session_store :active_record_store

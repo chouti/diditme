a0a6ec59b4d418ad5e098a67c60431f1a13a9529
chouti <chouti@gmail.com>
2011-03-13 15:12:36 +0800

    generate user_id migration

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,6 +1,11 @@
 class TaskController < ApplicationController
   before_filter :authenticate
   def index
+    @task=current_user.tasks.all
+    respond_to do |format|
+      format.html
+      format.json { render :json => @items}
+    end
   end
 
   def show
